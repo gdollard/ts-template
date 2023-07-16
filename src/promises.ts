c9b0d981a5dca52e0cef30211f35369567ead24f
Glenn Dollard <gdollard@cartrawler.com>
2023-07-16 13:56:10 +0100

    added stuff 2

--- src/promises.ts
+++ src/promises.ts
@@ -37,14 +37,24 @@ export const rejectPromise = async (throwIt: boolean): Promise<boolean> => {
       });
 }
 
-// some code to demo await vs return vs return await
-// based on this article: https://jakearchibald.com/2017/await-vs-return-vs-return-await/
-async function waitAndMaybeReject() {
-    // Wait one second
-    await new Promise((r) => setTimeout(r, 1000));
-    // Toss a coin
-    const isHeads = Boolean(Math.round(Math.random()));
-  
-    if (isHeads) return 'yay';
-    throw Error('Boo!');
-  }
+    // some code to demo await vs return vs return await
+    // based on this article: https://jakearchibald.com/2017/await-vs-return-vs-return-await/
+    async function waitAndMaybeReject() {
+        // Wait one second
+        await new Promise((r) => setTimeout(r, 1000));
+        // Toss a coin
+        const isHeads = Boolean(Math.round(Math.random()));
+    
+        if (isHeads) return 'yay';
+        throw Error('Boo!');
+    }
+
+    // Here, if you call foo, the returned promise will always fulfill with undefined, without waiting.
+    // Since we don't await or return the result of waitAndMaybeReject(), we don't react to it in any way. Code like this is usually a mistake.
+    async function foo() {
+    try {
+        waitAndMaybeReject();
+    } catch (e) {
+        return 'caught';
+    }
+    }
